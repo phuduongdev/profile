d4724de484c7f51694c78fc8ad9d6207f42c814c
phuduongdev <phu.duongtan@gmail.com>
2020-04-22 09:22:18 +0700

    update project

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import MetronomeApp from "./components/metronome/MetronomeApp";
 import WelcomeContent from "./components/profile/WelcomeContent";
 import RoadmapContent from "./components/profile/RoadmapContent";
 import Footer from "./components/common/Footer";
-import DictionaryView from "./components/esu-kun/DictionaryView";
 
 function App() {
   return (
     <div>
-      {/* <Nav />
+      <Nav />
       <WelcomeContent />
       <RoadmapContent />
       <MetronomeApp />
-      <Footer /> */}
-      <DictionaryView />
+      <Footer />
     </div>
   );
 }
